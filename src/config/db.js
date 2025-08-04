d889b11f81736fe10a95daf4dda21ac6c9581a78
billybahirwe <billybillybahirwe@gmail.com>
2025-08-04 12:03:35 +0300

    Add change the db.js file

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config();
+
+const mongoURI = process.env.MONGODB_URI || process.env.MONGO_URI;
+const dbName = process.env.DB_NAME;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(mongoURI, {
+      dbName: dbName,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log(`MongoDB connected to database: ${dbName}`);
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
@@ -16,3 +21,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
